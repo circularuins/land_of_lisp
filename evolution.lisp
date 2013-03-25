620c5196bba52e47894b4a4ee08c71aae1ad8514
和気 夏彦 <wake@air.local>
2013-03-26 00:15:39 +0900

    buzz

--- evolution.lisp
+++ evolution.lisp
@@ -3,4 +3,12 @@
 (defparameter *jungle* '(45 10 10 10))
 (defparameter *plant-energy* 80)
 
+;;; マップの植物
 (defparameter *plants* (make-hash-table :test #'equal))
+(defun random-plant (left top width height)
+  (let ((pos (cons (+ left (random width)) (+ top (random height)))))
+    (setf (gethash pos *plants*) t)))
+
+(defun add-plants ()
+  (apply #'random-plant *jungle*)
+  (random-plant 0 0 *width* *height*))
